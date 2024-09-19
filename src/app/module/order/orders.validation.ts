61ece1d8747ab27047bad3e61778c53f9ce30d6f
Afia Nahian Chowdhury <afia000nahian@gmail.com>
2024-09-20 01:30:50 +0600

    more features added

--- src/app/module/order/orders.validation.ts
+++ src/app/module/order/orders.validation.ts
@@ -1,13 +1,13 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const OrderValidationSchema = z.object({
-    email: z.string().email({ message: 'valid email required' }),
-    productId: z.string({
-        required_error: 'Product id must be given',
-        invalid_type_error: 'Product id  must be a sting'
-    }),
-    price: z.number().nonnegative({ message: "Price cannot be negative" }),
-    quantity: z.number().min(1, { message: "At least order one item" }),
+  email: z.string().email({ message: 'valid email required' }),
+  productId: z.string({
+    required_error: 'Product id must be given',
+    invalid_type_error: 'Product id  must be a sting',
+  }),
+  price: z.number().nonnegative(),
+  quantity: z.number().min(1),
 });
 
 export default OrderValidationSchema;
